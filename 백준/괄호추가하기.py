1d32f2a62af8d39e9fb760dcba9328e206e706bb
unknown <nillcafri123@gmail.com>
2019-10-26 22:47:46 +0900

    괄호추가하기 풀었다아아

--- 백준/괄호추가하기.py
+++ 백준/괄호추가하기.py
@@ -1,35 +1,77 @@
-def confirm_odd(s, result):
+def combination(depth, st, result):
+    global m
+    if depth == m:
+        confirm(result)
 
-    # 두개를 선택할 것인가
-    if s > N-1:
-        print(result)
-    # 한개를 선택할 것인가
-
-    # s == 0에서 시작
     else:
-        for i in range(s, N, 2):
-            print(i, 'iiiiii')
-            result.append(temp[i: i+3])
-            if i+3 < N:
-                result.append(temp[i+3])
-            confirm_odd(i+4, result)
-            if i+3 < N:
-                result.pop()
+        for i in range(st, len(calcul)):
+            result.append(calcul[i])
+            combination(depth+1, i+2, result)
             result.pop()
-            result.append(temp[i])
-            if i+1 < N:
-                result.append(temp[i+1])
 
-        # i+4에서 시작
+def confirm(final):
+    global f_result
+    visited = [0] * N
+    final = final[:]
+    new_result = []
+    for f in range(len(final)):
+        for idx in range(3):
+            visited[final[f][idx]] = 1
+
+    for v in range(1, N):
+        if v-1 == 0 and not visited[v-1]:
+            new_result.append(contents[v-1])
+
+        if not visited[v-1] and visited[v]:
+            new_result.append(final.pop(0)[-1])
+        elif v-1 == 0 and visited[v-1]:
+            new_result.append(final.pop(0)[-1])
+        elif not visited[v]:
+            new_result.append(contents[v])
+    m_result = new_result[0]
+    for i in range(1, len(new_result), 2):
+        if new_result[i] == '+':
+            m_result += new_result[i + 1]
+        elif new_result[i] == '-':
+            m_result -= new_result[i + 1]
+        elif new_result[i] == '*':
+            m_result *= new_result[i + 1]
+
+    if m_result > f_result:
+        f_result = m_result
+
 
 N = int(input())
-nums = [0]*N
+contents = [0]*N
 temp = input()
 for i in range(N):
-    if not i % 2:
-        nums[i] = int(temp[i])
+    if i % 2:
+        contents[i] = temp[i]
     else:
-        nums[i] = temp[i]
+        contents[i] = int(temp[i])
+f_result = contents[0]
+for i in range(1, N, 2):
+    if contents[i] == '+':
+        f_result += contents[i+1]
+    elif contents[i] == '-':
+        f_result -= contents[i+1]
+    elif contents[i] == '*':
+        f_result *= contents[i+1]
 
-result = []
-confirm_odd(0, result)
+
+calcul = []
+for x in range(2, N, 2):
+    if contents[x-1] == '+':
+        calcul.append([x-2, x-1, x, contents[x-2]+contents[x]])
+    elif contents[x-1] == '-':
+        calcul.append([x-2, x-1, x, contents[x-2]-contents[x]])
+    elif contents[x-1] == '*':
+        calcul.append([x-2, x-1, x, contents[x-2]*contents[x]])
+
+
+
+for m in range(1, (N+1)//4+1):
+    result = []
+    combination(0, 0, result)
+
+print(f_result)
