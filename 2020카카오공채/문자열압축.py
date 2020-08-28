1bd5f97ab64bde641ffed7c7d27e2f380619c3d5
ichhjkim <nillcafri123@gmail.com>
2020-08-29 01:44:42 +0900

    [카카오공채] 카카오 String

--- 2020카카오공채/문자열압축.py
+++ 2020카카오공채/문자열압축.py
@@ -1,7 +1,43 @@
 def solution(s):
-    answer = 0
-    for num in range(len(s)//2):
-        same = []
-        result = []
+    answer = 100000
+
+    for i in range(1, (len(s)//2)+2):
+        splited = []
+        for j in range(len(s)//i+1):
+            t = s[j*i:(j+1)*i]
+            if t: splited.append(t)
+        count = []
+
+        letters = []
+        for x in splited:
+            if letters:
+                if x != letters[-1]:
+                    letters.append(x)
+                    count.append(0)
+                else:
+                    count[-1] += 1
+            else:
+                letters.append(x)
+                count.append(0)
+
+        result = ''
+        for c in range(len(count)):
+            if count[c] > 0:
+                result += str(count[c]+1) + letters[c]
+            else:
+                result += letters[c]
+        if len(result) < answer:
+            answer = len(result)
 
     return answer
+
+test = [
+    "aabbaccc",
+    "ababcdcdababcdcd",
+    "abcabcdede",
+    "abcabcabcabcdededededede",
+    "xababcdcdababcdcd"
+]
+for t in test:
+    print(solution(t))
+
